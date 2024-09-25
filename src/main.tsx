760c924aa003a7405eafed50e320fcabb563d6a9
june960427 <ljh975896@gmail.com>
2024-09-25 17:44:03 +0900

    🚀 Feat : 컨테이너 추가, 기본 css 수정, 라우터 구성

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>
   </QueryClientProvider>,
 )
