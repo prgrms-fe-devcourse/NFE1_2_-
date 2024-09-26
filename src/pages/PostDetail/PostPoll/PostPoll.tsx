3153c813e24e3c7d902fa676b63e931ff919bda4
june960427 <ljh975896@gmail.com>
2024-09-26 14:25:51 +0900

    🚀 Feat : '투표 섹션 마크업 완료'

--- src/pages/PostDetail/PostPoll/PostPoll.tsx
+++ src/pages/PostDetail/PostPoll/PostPoll.tsx
@@ -1,10 +1,18 @@
+import { useCallback, useState } from 'react'
+import AfterPoll from './AfterPoll'
 import BeforePoll from './BeforePoll'
 import './PostPoll.css'
+
 const PostPoll = () => {
+  const [isPoll, setIsPoll] = useState(false)
+  const handleOnclick = useCallback(() => {
+    setIsPoll(true)
+  }, [])
+
   return (
     <div className='poll-container'>
       <h3 className='poll-title'>다시 연락할까요?</h3>
-      <BeforePoll />
+      {isPoll ? <AfterPoll /> : <BeforePoll onClick={handleOnclick} />}
       <p className='poll-subtitle'>의견을 선택하고 댓글을 확인해보세요!</p>
     </div>
   )
