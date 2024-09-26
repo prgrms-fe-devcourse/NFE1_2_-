257ee2008f9743c1975d11625d797dc1a958e09a
ahcgnoej <jinjinchachajung@naver.com>
2024-09-27 05:15:10 +0900

    🗑️ Chore : test

--- src/pages/PostDetail/PostDetail.tsx
+++ src/pages/PostDetail/PostDetail.tsx
@@ -1,10 +1,11 @@
 import Container from '@components/Conatiner/Container'
-
+import MyPage from '../MyPage/MyPage'
 const PostDetail = () => {
   return (
     <Container>
       <section>
         <p>Post Page</p>
+        <MyPage></MyPage>
       </section>
     </Container>
   )
