ec1487e077a5278708082b3db6593582baf7f1e9
eunjju2 <graceun309@gmail.com>
2024-09-26 22:10:37 +0900

    🎨 Style : 카테고리 드롭다운 스타일 수정

--- src/pages/PostCreate/CategorySelect/CategorySelect.tsx
+++ src/pages/PostCreate/CategorySelect/CategorySelect.tsx
@@ -10,19 +10,24 @@ const CategorySelect = () => {
   return (
     <div className='category'>
       <div className='category-container'>
-        <p className='category-title'>카테고리 선택</p>
-        <button className='category-button' onClick={() => setShowlist(!showlist)}>
+        <button
+          className='category-button'
+          onClick={() => setShowlist(!showlist)}
+        >
+          <p className='category-title'>카테고리 선택</p>
           {showlist ? <DropUpButton /> : <DropDownButton />}
         </button>
       </div>
       {showlist && (
-        <ul className='category-list'>
-          <li>이별</li>
-          <li>짝사랑</li>
-          <li>썸</li>
-          <li>데이트</li>
-          <li>기타</li>
-        </ul>
+        <div className='category-list-container'>
+          <ul className='category-list'>
+            <li>이별</li>
+            <li>짝사랑</li>
+            <li>썸</li>
+            <li>데이트</li>
+            <li>기타</li>
+          </ul>
+        </div>
       )}
     </div>
   )
