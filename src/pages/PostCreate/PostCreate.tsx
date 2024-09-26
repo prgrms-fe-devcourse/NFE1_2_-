747fc14493b6d40166273e1c407f0a1a626cf4ab
eunjju2 <graceun309@gmail.com>
2024-09-26 17:22:06 +0900

    🚀 Feat : 페이지 구조 배치 완료

--- src/pages/PostCreate/PostCreate.tsx
+++ src/pages/PostCreate/PostCreate.tsx
@@ -1,13 +1,25 @@
-import CategorySelect from "./CategorySelect"
-import PostContent from "./PostContent"
+import CategorySelect from './CategorySelect'
+import PostContent from './PostContent'
+import PostCreateButton from './PostCreateButton'
+import QuestionSelect from './QuestionSelect'
 
 import './PostCreate.css'
+import PostPageButton from '@/components/PostPageButton/PostPageButton'
 
 const PostCreate = () => {
+  const handleAddPhoto = () => {}
   return (
     <div className='post-create'>
       <CategorySelect />
       <PostContent />
+      <div className='add-image'>
+        <div className='add-image-container'>
+          <p className='add-image-name'>ddd.jpg</p>
+          <PostPageButton onClick={handleAddPhoto} title='사진추가'/>
+        </div>
+      </div>
+      <QuestionSelect />
+      <PostCreateButton />
     </div>
   )
 }
---
+++ src/pages/PostCreate/PostCreateButton.tsx
@@ -0,0 +1,11 @@
+import './PostCreateButton.css'
+
+const PostCreateButton = () => {
+  return (
+    <div className='post-create-button-container'>
+      <button className='post-create-button'>글쓰기</button>
+    </div>
+  );
+};
+
+export default PostCreateButton;
