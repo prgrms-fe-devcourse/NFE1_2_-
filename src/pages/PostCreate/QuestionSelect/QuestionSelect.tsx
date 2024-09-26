749eba68a7acd7a4cefed7cb541a540f172d3f6b
eunjju2 <graceun309@gmail.com>
2024-09-26 22:16:51 +0900

    🚀 Feat : 질문선택 버튼 클릭 시 화면 변경 구현

--- src/pages/PostCreate/QuestionSelect/QuestionSelect.tsx
+++ src/pages/PostCreate/QuestionSelect/QuestionSelect.tsx
@@ -6,13 +6,18 @@ const QuestionSelect = () => {
   return (
     <div className='question-select'>
       {isSelect ? (
-        <button onClick={() => setQuestion(true)} className='question-select-button'>질문 선택</button>
-      ) : (
         <div className='question-selected'>
           <p className='question-selected-title'>선택한 질문</p>
           <div className='question-selected-name'>사과할까요?</div>
           <button className='question-reselect'>질문 다시 선택</button>
         </div>
+      ) : (
+        <button
+          onClick={() => setQuestion(true)}
+          className='question-select-button'
+        >
+          질문 선택
+        </button>
       )}
     </div>
   )
