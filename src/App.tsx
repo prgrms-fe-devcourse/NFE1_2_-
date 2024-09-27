8f921ed44b9f057fb0d14d13c0fa11838fa14b7c
hyejun <g89g6@naver.com>
2024-09-27 18:05:42 +0900

    🚀 Feat : 404페이지 구현 (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import NotFound from '@pages/NotFound/NotFound'
 import PostDetail from '@pages/PostDetail/PostDetail'
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <Routes>
+      <Route 
+        path="*" 
+        element={<NotFound />} />
       <Route
         path='/postdetail'
         element={<PostDetail />}
