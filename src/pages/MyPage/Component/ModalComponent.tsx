cea431ea30973fe855e81f2ae3b2f1035335cd2a
ahcgnoej <jinjinchachajung@naver.com>
2024-09-28 14:30:16 +0900

    🚀 Feat : 마크업 완료 (코드 정리 전)

--- src/pages/MyPage/Component/ModalComponent.tsx
+++ src/pages/MyPage/Component/ModalComponent.tsx
@@ -1,5 +1,7 @@
 import BottomModal from '@/components/BottomModal/BottomModal'
 import '../MyPage.css'
+import MbtiSelector from '@/components/MbtiSelector/MbtiSelector'
+import SearchButton from '@assets/icons/list_search.svg?react'
 
 interface ModalSectionProps {
   isOpen: boolean
@@ -7,11 +9,15 @@ interface ModalSectionProps {
   buttonText: string
   detail?: string
   instruction: string
+  filter?: boolean
+  isMbtiFilterVisible?: boolean
+  onToggleFilter?: () => void
   inputFields: {
     label: string
     value: string
     handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
     type?: string
+    placeholder?: string
   }[]
 }
 
@@ -19,9 +25,12 @@ const ModalComponent = ({
   isOpen,
   onClose,
   buttonText,
-  detail = '',
+  detail,
+  filter,
   instruction,
   inputFields,
+  isMbtiFilterVisible,
+  onToggleFilter,
 }: ModalSectionProps) => {
   return (
     <>
@@ -33,16 +42,44 @@ const ModalComponent = ({
           <div className='modal-section'>
             <p className='modal-instruction'>{instruction}</p>
             <p className='modal-label'>{detail}</p>
+            {filter && (
+              <>
+                <div className='toggle-container'>
+                  <label className='toggle-label'>MBTI 필터링</label>
+                  <div
+                    className={`toggle-slider ${isMbtiFilterVisible ? 'active' : ''}`}
+                    onClick={onToggleFilter}
+                  >
+                    <div
+                      className={`slider ${isMbtiFilterVisible ? 'active' : ''}`}
+                    ></div>
+                  </div>
+                </div>
+                {isMbtiFilterVisible && (
+                  <div className='modal-filter'>
+                    <div>
+                      <MbtiSelector />
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
             <div className='modal-inner-section'>
               {inputFields.map((field, index) => (
                 <div key={index}>
                   <p className='modal-label'>{field.label}</p>
                   <input
                     type={field.type || 'text'}
-                    className='modal-input'
+                    className={filter ? 'modal-input-filter' : 'modal-input'}
                     value={field.value}
                     onChange={field.handleChange}
+                    placeholder={field.placeholder}
                   />
+                  {filter && (
+                    <button className='search-button'>
+                      <SearchButton />
+                    </button>
+                  )}
                 </div>
               ))}
             </div>
--- src/pages/PostList/Search/Search.tsx
+++ src/pages/PostList/Search/Search.tsx
@@ -1,34 +1,46 @@
-import MbtiSelector from '@/components/MbtiSelector/MbtiSelector'
 import ModalComponent from '@/pages/MyPage/Component/ModalComponent'
 import { useState } from 'react'
+import SearchButton from '@assets/icons/list_search.svg?react'
 
 const Search = ({ onClose }) => {
   const [search, setSearch] = useState('')
+  const [isMbtiFilterVisible, setIsMbtiFilterVisible] = useState(false)
 
   const handleCloseModal = () => {
     onClose()
     setSearch('')
+    setIsMbtiFilterVisible(false)
   }
 
   const inputFields = [
     {
       label: '',
-      value: '',
+      value: search,
       handleChange: (e: React.ChangeEvent<HTMLInputElement>) =>
         setSearch(e.target.value),
       type: 'text',
+      placeholder: '검색어를 입력해주세요',
     },
   ]
 
+  const handleToggleFilter = () => {
+    setIsMbtiFilterVisible((prev) => !prev)
+  }
+
   return (
-    <ModalComponent
-      isOpen={true}
-      onClose={handleCloseModal}
-      buttonText={'닫기'}
-      instruction='검색'
-      detail='검색할 포스트 제목을 입력하거나, MBTI 필터링 기능을 이용해보세요.'
-      inputFields={inputFields}
-    />
+    <>
+      <ModalComponent
+        isOpen={true}
+        onClose={handleCloseModal}
+        buttonText={'닫기'}
+        instruction='검색'
+        detail='검색할 포스트 제목을 입력하거나, MBTI 필터링 기능을 이용해보세요.'
+        filter={true}
+        inputFields={inputFields}
+        isMbtiFilterVisible={isMbtiFilterVisible}
+        onToggleFilter={handleToggleFilter}
+      />
+    </>
   )
 }
 
