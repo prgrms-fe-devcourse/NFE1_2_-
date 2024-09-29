c394b31f5ebb338c4400eb0c35e7519e603e0436
june960427 <ljh975896@gmail.com>
2024-09-29 21:33:05 +0900

    🔧 Fix : 타입 오류 수정

--- src/utils/formatPostData.tsx
+++ src/utils/formatPostData.tsx
@@ -1,19 +1,6 @@
-import { Post, Like, Channel, User } from '@/typings/types'
+import { Post } from '@/typings/types'
 
-interface PostData {
-  likes: Like[]
-  comments: Comment[]
-  _id: string
-  image?: string // 선택적
-  imagePublicId?: string // 선택적
-  title: string
-  channel: Channel
-  author: User
-  createdAt: string
-  updatedAt: string
-}
-
-const formatPostData = (data: PostData) => {
+const formatPostData = (data: Post) => {
   const { title, author } = data
 
   let userData
