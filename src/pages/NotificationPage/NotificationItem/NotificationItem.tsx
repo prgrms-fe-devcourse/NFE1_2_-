1484eb9353423004599b6251723c521d388d1200
eunjju2 <graceun309@gmail.com>
2024-09-30 22:32:12 +0900

    🚀 Feat : 알림별 분리해서 마크업 구현 완료

--- src/pages/NotificationPage/NotificationItem/NotificationItem.tsx
+++ src/pages/NotificationPage/NotificationItem/NotificationItem.tsx
@@ -4,34 +4,45 @@ import Popular from '@assets/icons/notification_congratulation.svg?react'
 import Comment from '@assets/icons/notification_comment.svg?react'
 import Like from '@assets/icons/notification_like.svg?react'
 import formatTime from '@/utils/formatTime'
+import { useCallback } from 'react'
 
 interface NotificationItemProps {
   notification: Notification
 }
 
+interface NotificationData {
+  notificationIcon: JSX.Element
+  notificationText: string
+}
+
 const NotificationItem = ({ notification }: NotificationItemProps) => {
-  const notificationIcon = () => {
+  const setNotificationData = useCallback<() => NotificationData>(() => {
     if (notification.like !== undefined) {
-      return <Like />
+      return {
+        notificationIcon: <Like />,
+        notificationText: `"왜만나는거야? 왜만나는거야?" 게시글에 좋아요가 달렸어요.`,
+      }
     } else if (notification.comment !== undefined) {
-      return <Comment />
+      return {
+        notificationIcon: <Comment />,
+        notificationText: `"왜만나는거야? 왜만나는거야? 왜만나는거야? 왜만나는거야?"에 댓글이 달렸어요.`,
+      }
     }
-    return <Popular />
-  }
-  const notificationText = () => {
-    if (notification.like !== undefined) {
-      return `~에 좋아요가 달렸어요.`
-    } else if (notification.comment !== undefined) {
-      return `~에 댓글이 달렸어요.`
+    return {
+      notificationIcon: <Popular />,
+      notificationText: `축하합니다! 회원님의 게시글이 인기글에 선정되었어요!`,
     }
-  }
+  }, [])
+
+  const { notificationIcon, notificationText } = setNotificationData()
   const notificationTime = formatTime(notification.createdAt)
+
   return (
     <div className='notification-item-container'>
       <div className='notification-item'>
-        {notificationIcon()}
+        {notificationIcon}
         <div className='notification-text-container'>
-          <p className='notification-text'>{notificationText()}</p>
+          <p className='notification-text'>{notificationText}</p>
           <p className='notification-time'>{notificationTime}</p>
         </div>
       </div>
