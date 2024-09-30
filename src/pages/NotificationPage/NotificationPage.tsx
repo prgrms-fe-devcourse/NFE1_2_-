e8322807592f6b929e41b3e015d618ac76cdc83d
eunjju2 <graceun309@gmail.com>
2024-09-30 18:00:14 +0900

    🚀 Feat : 시간별로 알림 분리

--- src/pages/NotificationPage/NotificationPage.tsx
+++ src/pages/NotificationPage/NotificationPage.tsx
@@ -5,6 +5,8 @@ import NotificationContainer from './NotificationContainer/NotificationContainer
 import { Notification } from '@/typings/types'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
+import formatTime from '@/utils/formatTime'
+import timeSeparation from '@/utils/timeSeparation'
 
 const getNotification = async (): Promise<Notification[]> => {
   const token =
@@ -40,25 +42,84 @@ const NotificationPage = () => {
   }
 
   console.log(data)
-  const unreadNotifications = data?.filter((notification) => !notification.seen)
+  const unreadNotifications: Notification[] | undefined = data?.filter(
+    (notification) => !notification.seen,
+  )
 
   console.log(unreadNotifications)
 
-  const likeNotification = data?.filter((notification) => notification?.like !== undefined)
-  const commentNotification = data?.filter((notification) => notification?.comment !== undefined)
+  const todayNotifications: Notification[] | undefined = []
+  const yesterdayNotifications: Notification[] | undefined = []
+  const weekNotifications: Notification[] | undefined = []
+  const pastNotifications: Notification[] | undefined = []
 
-  console.log('좋아요알림',likeNotification )
-  console.log('댓글알림',commentNotification )
+  data?.forEach((notification) => {
+    const timeNotification = timeSeparation(notification.createdAt)
+    if (timeNotification === 'TODAY') {
+      todayNotifications?.push(notification)
+    } else if (timeNotification === 'YESTERDAY') {
+      yesterdayNotifications?.push(notification)
+    } else if (timeNotification === 'WEEK') {
+      weekNotifications?.push(notification)
+    } else if (timeNotification === 'PAST') {
+      pastNotifications?.push(notification)
+    }
+  })
+
+  console.log('오늘', todayNotifications)
+  console.log('어제', yesterdayNotifications)
+  console.log('7일', weekNotifications)
+  console.log('과거', pastNotifications)
+
+  // const likeNotification = data?.filter(
+  //   (notification) => notification?.like !== undefined,
+  // )
+  // const commentNotification = data?.filter(
+  //   (notification) => notification?.comment !== undefined,
+  // )
+
+  // console.log('좋아요알림', likeNotification)
+  // console.log('댓글알림', commentNotification)
+
+  // console.log(
+  //   likeNotification?.map((notification) => formatTime(notification.createdAt)),
+  // )
 
   return (
     <DetailPageLayout
       pageName='notification'
       pageText='알림'
     >
-      <NotificationContainer period='읽지 않음' />
-      <NotificationContainer period='어제' />
-      <NotificationContainer period='최근 7일' />
-      <NotificationContainer period='이전 활동' />
+      {unreadNotifications && unreadNotifications.length > 1 && (
+        <NotificationContainer
+          period='읽지 않음'
+          notifications={unreadNotifications}
+        />
+      )}
+      {todayNotifications.length > 1 && (
+        <NotificationContainer
+          period='오늘'
+          notifications={todayNotifications}
+        />
+      )}
+      {yesterdayNotifications.length > 1 && (
+        <NotificationContainer
+          period='어제'
+          notifications={yesterdayNotifications}
+        />
+      )}
+      {weekNotifications.length > 1 && (
+        <NotificationContainer
+          period='최근 7일'
+          notifications={weekNotifications}
+        />
+      )}
+      {pastNotifications.length > 1 && (
+        <NotificationContainer
+          period='이전 활동'
+          notifications={pastNotifications}
+        />
+      )}
       <Navigator />
     </DetailPageLayout>
   )
