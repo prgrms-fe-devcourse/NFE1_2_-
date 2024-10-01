d2ccbafdc4d6802cd5116b038faa258ebccaf788
hyejun <g89g6@naver.com>
2024-10-01 11:04:29 +0900

    🗑️ Chore : 로그인 api콜 개발, 라우트 설정, 애러메시지 input창 하단 출력력

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import PostDetail from '@pages/PostDetail/Post/Post'
 import MyPage from './pages/MyPage/MyPage'
 import { Routes, Route } from 'react-router-dom'
-
+import LoginPage from './pages/LoginPage/LoginPage'
 const App = () => {
   return (
     <Routes>
+      <Route 
+        path="/login" 
+        element={<LoginPage />} />
       <Route
         path='/postdetail'
         element={<PostDetail />}
