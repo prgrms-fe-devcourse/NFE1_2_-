ce852bacbab1a3f7f44a3ed485a0785d88462f80
ahcgnoej <jinjinchachajung@naver.com>
2024-10-01 16:47:25 +0900

    🚀 Feat : List추가

--- src/pages/PostList/List.tsx
+++ src/pages/PostList/List.tsx
@@ -2,13 +2,33 @@ import Carousel from './Carousel/Carousel'
 import MainPageLayout from '@/layouts/MainPageLayout/MainPageLayout'
 import PreviewPostList from './PreviewPostList/PreviewPostList'
 import FilterSection from './FilterSection/FilterSection'
+import { useState } from 'react'
+
+//유저인증가져와야댐 일단은 그냥 아이디 적기
+
 const PostList = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [isCollectionActive, setIsCollectionActive] = useState(false)
+  const [authorId, setAuthorId] = useState(null)
+  const [selectedSort, setSelectedSort] = useState('popular') // 추가된 상태
+
   return (
     <MainPageLayout>
       <section>
-        <Carousel></Carousel>
-        <FilterSection></FilterSection>
-        <PreviewPostList></PreviewPostList>
+        <Carousel setSelectedCategory={setSelectedCategory} />
+        <FilterSection
+          isCollectionActive={isCollectionActive}
+          setIsCollectionActive={setIsCollectionActive}
+          setAuthorId={setAuthorId}
+          setSelectedSort={setSelectedSort}
+        />
+        <PreviewPostList
+          channelId='66f6b3b7e5593e2a995daf1f'
+          selectedCategory={selectedCategory}
+          isCollectionActive={isCollectionActive}
+          authorId={authorId}
+          selectedSort={selectedSort}
+        />
       </section>
     </MainPageLayout>
   )
