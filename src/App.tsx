e05ad0c0ea4075ac49d87edff6c2f934d5ce905e
hyejun <g89g6@naver.com>
2024-10-03 00:21:18 +0900

    🔧 Fix : 새브렌치 생성하여 충돌 수정 (#20)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import PostDetailPage from '@pages/PostDetail/PostDetailPage'
 import MyPage from './pages/MyPage/MyPage'
 import PostCreate from './pages/PostCreate/PostCreate'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import NotificationPage from './pages/NotificationPage/NotificationPage'
 import PostList from './pages/PostList/List'
 import LoginPage from './pages/LoginPage/LoginPage'
+import SplashScreen from '@pages/SplashScreen/SplashScreen'
+import { useState, useEffect } from 'react'
 
 const App = () => {
+
+  const [showSplash, setShowSplash] = useState(true)
+
+  useEffect(() => {
+    // 세션 스토리지 사용
+    const hasVisitedBefore = sessionStorage.getItem('hasVisitedBefore')
+    if (hasVisitedBefore) {
+      setShowSplash(false)
+    }
+  }, [])
+
+  const handleSplashFinished = () => {
+    sessionStorage.setItem('hasVisitedBefore', 'true')
+    setShowSplash(false)
+  }
+
+  if (showSplash) {
+    return <SplashScreen onFinished={handleSplashFinished} />
+  }
+
   return (
     <Routes>
       <Route
@@ -32,6 +54,10 @@ const App = () => {
       <Route
         path='/'
         element={<PostList />}
+      />
+      <Route 
+        path="*" 
+        element={<Navigate to="/" replace />} 
       />
     </Routes>
   )
