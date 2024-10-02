60646e9d568f5073b8155c5b2af6d72e8ed3b52e
eunjju2 <graceun309@gmail.com>
2024-10-02 15:42:12 +0900

    🚀 Feat : 하단 모달창 중복 안되도록 수정

--- src/pages/MyPage/MyPageSection/AccountSection.tsx
+++ src/pages/MyPage/MyPageSection/AccountSection.tsx
@@ -3,15 +3,20 @@ import ModalComponent from '../Component/ModalComponent/ModalComponent'
 import InfoSection from '../Component/InfoSection/InfoSection'
 import '../MyPage.css'
 
-const AccountSection = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
+interface SectionProps {
+  isModalOpen: boolean
+  onChangeOpenModal: () => void
+  onChangeCloseModal: () => void
+}
+
+const AccountSection = (props: SectionProps) => {
+  const { isModalOpen, onChangeOpenModal, onChangeCloseModal } = props
   const [oldPassword, setOldPassword] = useState('')
   const [newPassword, setNewPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
 
-  const handleOpenModal = () => setIsModalOpen(true)
   const handleCloseModal = () => {
-    setIsModalOpen(false)
+    onChangeCloseModal()
     setOldPassword('')
     setNewPassword('')
     setConfirmPassword('')
@@ -24,40 +29,41 @@ const AccountSection = () => {
       </p>
       <button
         className='info-item'
-        onClick={handleOpenModal}
+        onClick={onChangeOpenModal}
       >
         비밀번호 변경
       </button>
-      <ModalComponent
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        buttonText={
-          oldPassword || newPassword || confirmPassword ? '확인' : '닫기'
-        }
-        instruction='새로운 비밀번호를 입력해주세요!'
-      >
-        <p className='modal-label'>기존 비밀번호</p>
-        <input
-          type='password'
-          className='modal-input'
-          value={oldPassword}
-          onChange={(e) => setOldPassword(e.target.value)}
-        />
-        <p className='modal-label'>새로운 비밀번호</p>
-        <input
-          type='password'
-          className='modal-input'
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-        />
-        <p className='modal-label'>비밀번호 확인</p>
-        <input
-          type='password'
-          className='modal-input'
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-      </ModalComponent>
+      {isModalOpen && (
+        <ModalComponent
+          onClose={handleCloseModal}
+          buttonText={
+            oldPassword || newPassword || confirmPassword ? '확인' : '닫기'
+          }
+          instruction='새로운 비밀번호를 입력해주세요!'
+        >
+          <p className='modal-label'>기존 비밀번호</p>
+          <input
+            type='password'
+            className='modal-input'
+            value={oldPassword}
+            onChange={(e) => setOldPassword(e.target.value)}
+          />
+          <p className='modal-label'>새로운 비밀번호</p>
+          <input
+            type='password'
+            className='modal-input'
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+          <p className='modal-label'>비밀번호 확인</p>
+          <input
+            type='password'
+            className='modal-input'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </ModalComponent>
+      )}
     </InfoSection>
   )
 }
