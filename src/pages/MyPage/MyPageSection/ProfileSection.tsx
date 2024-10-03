925cd44d5282ee2b00ee963226e7e19b9da2f764
eunjju2 <graceun309@gmail.com>
2024-10-03 13:29:19 +0900

    🚀 Feat : 프로필 수정 구현

--- src/pages/MyPage/MyPageSection/ProfileSection.tsx
+++ src/pages/MyPage/MyPageSection/ProfileSection.tsx
@@ -1,7 +1,9 @@
-import { User } from '@/typings/types'
+import { User, UserDetailData } from '@/typings/types'
 import InfoSection from '../Component/InfoSection/InfoSection'
 import '../MyPage.css'
 import { parseIfString } from '@/utils/formatPostData'
+import JoinDetail from '@/pages/JoinPage/JoinDetail/JoinDetail'
+import { updateUserData } from '@/utils/api'
 
 interface SectionProps {
   userData: User | undefined
@@ -12,21 +14,58 @@ interface SectionProps {
 
 const ProfileSection = (props: SectionProps) => {
   const { userData, isModalOpen, onChangeOpenModal, onChangeCloseModal } = props
-  const { gender, ageGroup, mbti } = parseIfString(userData.fullName)
+  const fullName = userData?.fullName
+  const { gender, ageGroup, mbti } = parseIfString(fullName as UserDetailData)
+
+  const calculateAgeGroup = (birthDate: string) => {
+    const today = new Date()
+    const birthYear = new Date(birthDate).getFullYear()
+    const age = today.getFullYear() - birthYear
+    const ageGroup = Math.floor(age / 10) * 10
+    return ageGroup >= 90 ? '90' : `${ageGroup}`
+  }
+
+  const handleEditProfile = (
+    gender: '남' | '여',
+    birthDate: string,
+    mbti: string,
+  ) => {
+    const fullNameObject = {
+      gender: gender,
+      ageGroup: calculateAgeGroup(birthDate),
+      mbti: mbti,
+    }
+    const fullname = JSON.stringify(fullNameObject)
+    updateUserData(fullname)
+    console.log('변경완료', gender, birthDate, mbti)
+  }
 
   return (
-    <InfoSection title='프로필'>
-      <p className='info-item'>
-        MBTI<span className='value'>{mbti}</span>
-      </p>
-      <p className='info-item'>
-        성별<span className='value'>{gender}</span>
-      </p>
-      <p className='info-item'>
-        나이<span className='value'>{ageGroup}대</span>
-      </p>
-      <div className='info-edit'>프로필 수정</div>
-    </InfoSection>
+    <>
+      <InfoSection title='프로필'>
+        <p className='info-item'>
+          MBTI<span className='value'>{mbti}</span>
+        </p>
+        <p className='info-item'>
+          성별<span className='value'>{gender}</span>
+        </p>
+        <p className='info-item'>
+          나이<span className='value'>{ageGroup}대</span>
+        </p>
+        <button
+          className='info-edit'
+          onClick={onChangeOpenModal}
+        >
+          프로필 수정
+        </button>
+      </InfoSection>
+      {isModalOpen && (
+        <JoinDetail
+          onSubmit={handleEditProfile}
+          onClose={onChangeCloseModal}
+        />
+      )}
+    </>
   )
 }
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -39,6 +39,21 @@ export const getUserData = async (userId: string): Promise<User> => {
   }
 }
 
+export const updateUserData = async (fullname: string): Promise<void> => {
+  try {
+    const response = await axios.put(
+      `${END_POINT}settings/update-user`,
+      { "fullName" : fullname,
+        "username" : "false"
+      },
+      RequestHeader,
+    )
+    return response.data
+  } catch (error) {
+    throw handleError(error)
+  }
+}
+
 export const updateUserPassword = async (newPassword: string): Promise<void> => {
   try {
     const response = await axios.put(
