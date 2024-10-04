7481031f62b5dbb5946405f4a54a1db4358836a5
eunjju2 <graceun309@gmail.com>
2024-10-04 21:48:38 +0900

    🚀 Feat : checkCount 수정 및 네비게이터 경로 수정

--- src/pages/PostCreate/PostCreate.tsx
+++ src/pages/PostCreate/PostCreate.tsx
@@ -57,14 +57,12 @@ const PostCreate = () => {
     const postData = formatPostData(post)
     const isPollAgree = postData.title.poll.agree
     const isPollDisAgree = postData.title.poll.disagree
-    console.log(post)
-    console.log(postData)
-
     setPostData((prevState) => ({
       ...prevState,
       ['type']: postData.title.type,
       ['title']: postData.title.title,
       ['body']: postData.title.body,
+      ['checkCount'] : postData.title.checkCount,
       ['poll']: postData.title.poll,
     }))
     if (postData.image) {
@@ -110,7 +108,7 @@ const PostCreate = () => {
   return isLoading ? (
     <Loading />
   ) : (
-    <DetailPageLayout>
+    <DetailPageLayout newPath={true}>
       <div className='post-create'>
         <CategorySelect
           category={postData.type}
