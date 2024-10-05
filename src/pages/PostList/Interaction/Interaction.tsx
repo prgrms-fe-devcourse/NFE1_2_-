ea50cc28590b09cf687d68e160965d73b3268e3d
june960427 <ljh975896@gmail.com>
2024-10-05 20:54:41 +0900

    🎨 Style : 로그인, 회원가입 페이지 css 수정

--- src/pages/PostList/Interaction/Interaction.tsx
+++ src/pages/PostList/Interaction/Interaction.tsx
@@ -13,30 +13,28 @@ const Interaction = ({ post }: { post: FormattedPost }) => {
   const votedUserLength = poll.agree.length + poll.disagree.length
 
   return (
-    <div>
-      <div className='interaction-icon'>
-        <div className='icon-value'>
-          <ListVote
-            width={19}
-            height={19}
-          ></ListVote>
-          <span>{votedUserLength}</span>
-        </div>
-        <div className='icon-value'>
-          <ListComment
-            width={18}
-            height={18}
-          ></ListComment>
-          <span>{comments.length}</span>
-        </div>
-        <div className='icon-value'>
-          <HeartBefore
-            width={16}
-            height={16}
-            fill='#7d7d7d'
-          ></HeartBefore>
-          <span>{likes.length}</span>
-        </div>
+    <div className='interaction-icon'>
+      <div className='icon-value'>
+        <ListVote
+          width={19}
+          height={19}
+        ></ListVote>
+        <span>{votedUserLength}</span>
+      </div>
+      <div className='icon-value'>
+        <ListComment
+          width={18}
+          height={18}
+        ></ListComment>
+        <span>{comments.length}</span>
+      </div>
+      <div className='icon-value'>
+        <HeartBefore
+          width={16}
+          height={16}
+          fill='#7d7d7d'
+        ></HeartBefore>
+        <span>{likes.length}</span>
       </div>
     </div>
   )
